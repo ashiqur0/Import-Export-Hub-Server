7c3285cccc97d82b32d073f7d3f16e9586d56e1a
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-11-12 10:02:16 +0600

    MongoDB Connect With the Project

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello from server');
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.edix7i0.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client 
+        await client.connect();
+
+        // create database
+        const db = client.db('import_export_db');
+        const productsCollection = db.collection();
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.listen(port, (req, res) => {
     console.log(`Server is running at http://localhost:${port}`);
