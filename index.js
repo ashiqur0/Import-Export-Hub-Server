c891d618dcee1b9359ff325ba8942450ea88586b
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-11-18 19:15:33 +0600

    Add PATCH api for products databse and POST and GET api for import database

--- index.js
+++ index.js
@@ -32,36 +32,67 @@ async function run() {
 
         // create database collection
         const productsCollection = db.collection('products');
+        const importedProductCollection = db.collection('imported_product');
 
         // products related api
         // create product
-        app.post('/products', async(req, res) => {
+        app.post('/products', async (req, res) => {
             const newProduct = req.body;
             const result = await productsCollection.insertOne(newProduct);
             res.send(result);
         });
 
         // get latest product
-        app.get('/latest-products', async(req, res) => {
-            const cursor = productsCollection.find().sort({created_at: -1}).limit(6);
+        app.get('/latest-products', async (req, res) => {
+            const cursor = productsCollection.find().sort({ created_at: -1 }).limit(6);
             const result = await cursor.toArray();
             res.send(result);
         });
 
         // get all product
-        app.get('/products', async(req, res) => {
+        app.get('/products', async (req, res) => {
             const cursor = productsCollection.find();
             const result = await cursor.toArray();
             res.send(result);
         });
 
         // get individual product with id
-        app.get('/products/:id', async(req, res) => {
+        app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await productsCollection.findOne(query);
             res.send(result);
         });
+
+        // update product
+        app.patch('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const { availableQuantity } = req.body;
+            const query = { _id: new ObjectId(id) };
+            const update = {
+                $set: {
+                    availableQuantity: availableQuantity
+                }
+            }
+            const options = {};
+            const result = await productsCollection.updateOne(query, update, options);
+            res.send(result);
+        });
+
+        // import related api
+        // create imported product
+        app.post('/import', async (req, res) => {
+            const importedProduct = req.body;
+            const result = await importedProductCollection.insertOne(importedProduct);
+            res.send(result);
+        });
+
+        // get imported product
+        app.get('/import', async (req, res) => {
+            const cursor = importedProductCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
